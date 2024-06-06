02128497cf16476fc17e63ec11ca42b6fd0907b6
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-06 21:27:12 +0200

    TRy  const user = await Users.create(id,{image:req.file.filename})

--- server/routes/users.js
+++ server/routes/users.js
@@ -55,7 +55,8 @@ router.post('/:id', uploadImage.single('file'), async (req, res)=>{
     console.log(req.file.filename)
     console.log(id)
 
-    const user = await Users.findByIdAndUpdate(id,{image:req.file.filename})
+    // const user = await Users.findByIdAndUpdate(id,{image:req.file.filename})
+    const user = await Users.create(id,{image:req.file.filename})
 
     if(!user){
         return res.status(400).json({error: "No WorkOut found"})
