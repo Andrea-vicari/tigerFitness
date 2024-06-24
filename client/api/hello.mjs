5e67e8f8f2e3daa8745f85cbffd533ff18762bb4
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-24 19:09:50 +0200

    Try

--- client/api/hello.mjs
+++ client/api/hello.mjs
@@ -1,6 +1,15 @@
+
+
 export const config = {
   runtime: 'nodejs', // this is a pre-requisite
 };
+
+export default function handler(request, response) {
+  const { name = 'World' } = request.query;
+  return response.send(`Hello ${name}!`);
+}
+
+
 
 export default function handler(request, response) {
   return response.status(200).json({ text: 'I am a Serverless Function!' });
