3bb3510437ea17a347bb9f3b66ddadcb9c3cbff5
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-24 19:16:04 +0200

    oo

--- client/api/hello.mjs
+++ client/api/hello.mjs
@@ -3,11 +3,10 @@
 
 export default function handler(request, response) {
   const { name = 'World' } = request.query;
-  return response.send(`Hello ${name}!`);
-}
-
-
-
-export default function handler(request, response) {
+  response.send(`Hello ${name}!`);
   return response.status(200).json({ text: 'I am a Serverless Function!' });
+
 }
+
+
+
