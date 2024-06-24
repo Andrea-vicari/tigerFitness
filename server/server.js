c18c5869ba1205d7a8ed1895a69b7bbe15689651
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-24 17:01:38 +0200

    Try to add /images to app.use(express.static(path.join(__dirname, 'public/images')));

--- server/server.js
+++ server/server.js
@@ -9,11 +9,8 @@ const path = require('path');
 
 app.use(express.json());
 // app.use(express.static('public'))
-app.use(express.static(path.join(process.cwd(), 'public')))
-
-
-// Add comment
-//app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(process.cwd(), 'public')))
+app.use(express.static(path.join(__dirname, 'public/images')));
 
 app.use(cors(
     {
