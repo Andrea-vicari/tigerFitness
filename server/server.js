d61529ed8f678bbbbe1280e0598cd0f68f82cd3f
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-25 10:18:20 +0200

    TRy app.use('public', express.static(path.join(__dirname,'public')));

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // app.use(express.static(path.join(process.cwd(), 'public')))
 
-app.use('/public', express.static(path.join(__dirname,'public')));
+app.use('public', express.static(path.join(__dirname,'public')));
 
 
 
