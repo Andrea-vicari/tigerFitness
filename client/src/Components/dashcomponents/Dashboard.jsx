6b015ff556b026c998b9bbb8adddd56193987646
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-25 12:50:41 +0200

    Add image to dashboard

--- client/src/Components/dashcomponents/Dashboard.jsx
+++ client/src/Components/dashcomponents/Dashboard.jsx
@@ -42,14 +42,30 @@ function Dashboard() {
 
                             {/** Below only for ADMINS */}
                             {role == "admin" &&
+
                             <div className="col-md-9 mt-5">
-                                <div class="col-md-4">
-                                    <img src={userlistImg} class="img-fluid rounded-start" alt="..."/>
+                                <div className="col-md-4">
+                                    <img src={userlistImg} className="img-fluid rounded-start" alt="..."/>
                                     </div>
                                 <div className="p-5 bg-body-tertiary border rounded-3">
                                     <h2>Lista Utenti</h2>
                                     <p>Vedi l'elenco completo degli utenti da dove potrai vedere/aggiungere i workout di ogni singolo utente </p>
                                     <Link to="/userslist" type="button" className="btn btn-sm btn-outline-primary">Vai alla lista utenti</Link>
+                                </div>
+                            </div>}
+                            {/** Below only for ADMINS */}
+                            {role == "admin" &&
+
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                <img src={userlistImg} className="img-fluid rounded-start" alt="..."/>
+                                </div>
+                                <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                                </div>
                                 </div>
                             </div>}
                             {/** Below only for USERS */}
