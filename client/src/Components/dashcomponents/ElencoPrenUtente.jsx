fe7f5e2be29ce412a783492ac01736befeeb4611
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-26 17:20:24 +0200

    Change layout (Album style) to ElencoPrenUtente

--- client/src/Components/dashcomponents/ElencoPrenUtente.jsx
+++ client/src/Components/dashcomponents/ElencoPrenUtente.jsx
@@ -27,130 +27,63 @@ function ElencoPrenUtente() {
         <React.Fragment>
                 <div className='container-fluid pt-1 mt-5 bg-stripe'>
                     <div className='container text-center mt-5 pb-1'>
-                        <h1 className='display-2 text-white text-uppercase'>ElencoPrenUtente</h1>
+                        <h1 className='display-2 text-white text-uppercase'>Elenco Prenotazioni</h1>
                     </div>
                 </div>
             <section className={"py-3" + " " + bgType + " " + textType}>
 
                 <div className="container">
-                    <h1 className="section-title pt-5">{role} ElencoPrenUtente</h1>
-                    <div className="container">
-                        <div className="row mb-3">
-                            <div className="col-md-3">
-                                <h2 className="fw-bold text-body-emphasis">Area Gestionale</h2>
-                                <UserProfile />
+                    <h1 className="section-title pt-5">Elenco Prenotazioni</h1>
+                <div className="container">
 
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        <div className="col">
+                        <div className="card shadow-sm">
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            <div className="card-body">
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="btn-group">
+                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small className="text-body-secondary">9 mins</small>
                             </div>
-
-                            {/** Below only for ADMINS */}
-                            {role == "admin" &&
-
-                            <div className="col-md-9 mt-5">
-
-                                <div className="p-3 bg-body-tertiary border rounded-3">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={agendaImg} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Agenda</h2>
-                                                <p>Vedi l'elenco completo degli utenti da dove potrai vedere/aggiungere i workout di ogni singolo utente </p>
-                                                <Link to="/agenda" type="button" className="btn btn-sm btn-outline-primary">Vai alla lista utenti</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-
-                                <div className="p-3 bg-body-tertiary border rounded-3">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={userlistImg} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Lista Utenti</h2>
-                                                <p>Vedi l'elenco completo degli utenti da dove potrai vedere/aggiungere i workout di ogni singolo utente </p>
-                                                <Link to="/userslist" type="button" className="btn btn-sm btn-outline-primary">Vai alla lista utenti</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-                                <div className="p-3 bg-body-tertiary border rounded-3 mt-2">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={elencoPren} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Elenco Prenotazioni</h2>
-                                                <p>Vedi l'elenco completo degli utenti da dove potrai vedere/aggiungere i workout di ogni singolo utente </p>
-                                                <Link to="/userslist" type="button" className="btn btn-sm btn-outline-primary">Vai alla lista utenti</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-                                <div className="p-3 bg-body-tertiary border rounded-3 mt-2">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={newTrainingImg} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Aggiungi Workout</h2>
-                                                <p>Vedi l'elenco completo degli utenti da dove potrai vedere/aggiungere i workout di ogni singolo utente </p>
-                                                <Link to="/userslist" type="button" className="btn btn-sm btn-outline-primary">Aggiungi Workout</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-                            </div>}
-
-                            {/** Below only for USERS */}
-                            {role == "user" &&
-                            <div className="col-md-9 mt-5">
-                             <div className="p-3 bg-body-tertiary border rounded-3 mb-2">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={newTrainingImg} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Scheda Training</h2>
-                                                <p>Clicca per accedere alla scheda allenamento attiva</p>
-                                                <Link to="/elencoschedeapertepage" type="button" className="btn btn-sm btn-outline-primary">vai alla scheda</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-
-                                <div className="p-3 bg-body-tertiary border rounded-3 mb-2">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={agendaImg} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Prenota Lezione</h2>
-                                                <p>Prenota qui la tua prossima lezione</p>
-                                                <Link to="/newbooking" type="button" className="btn btn-sm btn-outline-primary">Prenota la lezione</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-                                <div className="p-3 bg-body-tertiary border rounded-3 mb-2">
-                                    <div className='row'>
-                                        <div className="col-md-3">
-                                            <img src={elencoPren} className="img-fluid rounded mb-2"/>
-                                            </div>
-                                            <div className="col-md-6">
-                                                <h2>Controlla le prenotazioni</h2>
-                                                <p>Controlla lo stato delle tue prenotazioni</p>
-                                                <Link to="/elencoprenotazioniutente" type="button" className="btn btn-sm btn-outline-primary">Controlla</Link>
-                                            </div>
-                                    </div>
-
-                                </div>
-
                             </div>
-
-                            }
-
                         </div>
+                        </div>
+                        <div className="col">
+                        <div className="card shadow-sm">
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            <div className="card-body">
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="btn-group">
+                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small className="text-body-secondary">9 mins</small>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col">
+                        <div className="card shadow-sm">
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                            <div className="card-body">
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="btn-group">
+                                <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small className="text-body-secondary">9 mins</small>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
+                    </div>
                     </div>
                 </div>
 
