ee46217cc30194307136b8f72ec62ac2eb4082f5
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-26 17:26:09 +0200

    ss

--- client/src/Components/dashcomponents/ElencoPrenUtente.jsx
+++ client/src/Components/dashcomponents/ElencoPrenUtente.jsx
@@ -38,19 +38,14 @@ function ElencoPrenUtente() {
 
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                         <div className="col">
-                        <div className="card shadow-sm">
-                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                            <div className="card-body">
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="btn-group">
-                                <button type="button" className="btn btn-sm btn-outline-secondary mx-2">View</button>
-                                <button type="button" className="btn btn-sm btn-outline-secondary mx-2">Edit</button>
+                            <div className="card">
+                                <h5 className="card-header">Featured</h5>
+                                <div className="card-body">
+                                    <h5 className="card-title">Special title treatment</h5>
+                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
                                 </div>
-
                             </div>
-                            </div>
-                        </div>
                         </div>
 
 
