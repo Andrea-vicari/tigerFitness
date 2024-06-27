1f1f3d8fb4f73be02e95e782069b0264c0c24c80
Andrea-vicari <andrea.vicari77@gmail.com>
2024-06-28 00:11:43 +0200

    ddqdqe

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -10,7 +10,7 @@ router.get('/', viewAllBookings);
 router.post('/', createNewBooking);
 router.get('/:id', viewSingleBooking)
 router.patch('/:id', updateStatusBooking)
-router.get('/tba', viewBookingTBA)
+router.get('/tba/tba', viewBookingTBA)
 
 
 module.exports = router;
