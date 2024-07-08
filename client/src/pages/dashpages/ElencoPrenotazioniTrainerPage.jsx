437b8b4b628d2d0e9a34f37b4dbd7d07ed68f6e7
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-08 15:27:31 +0200

    Create ElencoPrenotazioniTrainerPage.jsx
    Added from Dem Fedeli ElencoPrenotazioniTrainerPage

---
+++ client/src/pages/dashpages/ElencoPrenotazioniTrainerPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ElencoPrenUtente from '../../Components/dashcomponents/ElencoPrenUtente'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/NavbarFixedTop';
+
+
+function ElencoPrenotazioniTrainerPage() {
+  return (
+    <>
+    <Navbar />
+    <ElencoPrenUtente />
+    <Footer />
+    </>
+  )
+}
+
+export default ElencoPrenotazioniTrainerPage
