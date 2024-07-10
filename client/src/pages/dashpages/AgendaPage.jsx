57065b924714936ec495b53cdb622d08f7e5b2f5
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:49:41 +0200

    Update AgendaPage.jsx

--- client/src/pages/dashpages/AgendaPage.jsx
+++ client/src/pages/dashpages/AgendaPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
-import Navbar from '../../Components/Common/NavbarFixedTop';
+import Navbar from '../../Components/Common/DashNav';
 import Agenda from '../../Components/dashcomponents/Agenda';
 
 function AgendaPage() {
