8672f8368a265f48373e0c32a6c149407ed403aa
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:50:52 +0200

    Update ElencoSchedeChiusePage.jsx

--- client/src/pages/dashpages/ElencoSchedeChiusePage.jsx
+++ client/src/pages/dashpages/ElencoSchedeChiusePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
-import Navbar from '../../Components/Common/NavbarFixedTop';
+import Navbar from '../../Components/Common/DashNav';
 import ElencoSchedeChiuse from '../../Components/dashcomponents/ElencoSchedeChiuse';
 
 function ElencoSchedeChiusePage() {
