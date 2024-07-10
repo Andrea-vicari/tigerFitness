16ef117f11136d83cddff21d609d1669590e0bdc
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:08:07 +0200

    Update TrainerMenu.jsx

--- client/src/Components/Common/TrainerMenu.jsx
+++ client/src/Components/Common/TrainerMenu.jsx
@@ -8,16 +8,16 @@ function TrainerMenu() {
                 <Link className="nav-link fs-4" aria-current="page" to={'/'}>Home</Link>
               </li>
               <li className="nav-item">
-                <Link className={splitLocation[1] == "elencoschedeapertepage" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/elencoschedeapertepage'} >Prenotazioni confermate</Link>
+                <Link className={splitLocation[1] == "elencoprenotazionitrainer" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/agenda'} >Prenotazioni confermate</Link>
               </li>
               <li className="nav-item">
                 <Link className={splitLocation[1] == "userslist" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/userslist'}>Elenco utenti</Link>
               </li>
               <li className="nav-item">
-                <Link className={splitLocation[1] == "elencoprenotazioniutente" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/elencoprenotazioniutente'}>Controlla prenotazioni</Link>
+                <Link className={splitLocation[1] == "userslist" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/userslist'}>Aggiungi Workout</Link>
               </li>
               <li className="nav-item">
-                <Link className={splitLocation[1] == "elencoschedechiusepage" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/elencoschedechiusepage'}>Schede Completate</Link>
+                <Link className={splitLocation[1] == "elencoprenotazionitrainer" ? "active nav-link fs-4" : "nav-link fs-4"} to={'/elencoprenotazionitrainer'}>Prenotazioni confermate</Link>
               </li>
 
 
