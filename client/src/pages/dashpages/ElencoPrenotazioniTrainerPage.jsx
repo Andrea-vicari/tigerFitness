5ee2333ee69047dea4f16d23ab20ad0eb2e7680c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:50:07 +0200

    Update ElencoPrenotazioniTrainerPage.jsx

--- client/src/pages/dashpages/ElencoPrenotazioniTrainerPage.jsx
+++ client/src/pages/dashpages/ElencoPrenotazioniTrainerPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ElencoPrenTrainer from '../../Components/dashcomponents/ElencoPrenTrainer'
 import Footer from '../../Components/Common/Footer';
-import Navbar from '../../Components/Common/NavbarFixedTop';
+import Navbar from '../../Components/Common/DashNav';
 
 
 function ElencoPrenotazioniTrainerPage() {
