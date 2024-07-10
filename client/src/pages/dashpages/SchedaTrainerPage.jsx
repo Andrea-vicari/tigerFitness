035b4d6e02c74e1adc75cc84b04b87962b356e0b
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:51:33 +0200

    Update SchedaTrainerPage.jsx

--- client/src/pages/dashpages/SchedaTrainerPage.jsx
+++ client/src/pages/dashpages/SchedaTrainerPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
-import Navbar from '../../Components/Common/NavbarFixedTop';
+import Navbar from '../../Components/Common/DashNav';
 import SchedaTrainingTrainer from '../../Components/dashcomponents/SchedaTrainingTrainer';
 
 function SchedaTrainerPage() {
