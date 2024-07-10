93c4250cd6a71a0ebb74909c04054a6ec822fb91
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-10 11:50:38 +0200

    Update ElencoSchedeApertePage.jsx

--- client/src/pages/dashpages/ElencoSchedeApertePage.jsx
+++ client/src/pages/dashpages/ElencoSchedeApertePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
-import Navbar from '../../Components/Common/NavbarFixedTop';
+import Navbar from '../../Components/Common/DashNav';
 import ElencoSchedeAperte from '../../Components/dashcomponents/ElencoSchedeAperte';
 
 function ElencoSchedeApertePage() {
