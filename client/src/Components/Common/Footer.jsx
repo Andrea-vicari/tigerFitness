7f534f2290386d106b8b7220c78b4003c0aea66a
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-12 11:41:11 +0200

    Update Footer.jsx

--- client/src/Components/Common/Footer.jsx
+++ client/src/Components/Common/Footer.jsx
@@ -27,30 +27,28 @@ function Footer() {
 
   window.addEventListener('scroll', toggleVisible);
 
-  return (
-    <div className='container-fluid bg-primary py-4'>
-      <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center">
-          <p className="col-md-4 mb-0 text-white">© 2024 Company, Inc</p>
-
+return(
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center pt-3 mt-4 border-top bg-dark">
+                <div className="col-md-4 d-flex align-items-center">
           <Link className="navbar-brand d-flex" to={'/'}>
             <img src={logo} style={{width:150}}/>
           </Link>
+                <span className="mb-3 mb-md-0 text-white-50">© 2024 Fitness Club</span>
+                </div>
 
-          <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><Link to="/" className="nav-link px-2 text-white">Home</Link></li>
-            <li className="nav-item"><Link to="/aboutus" className="nav-link px-2 text-white">About</Link></li>
-            <li className="nav-item"><Link to="/faqpage" className="nav-link px-2 text-white">FAQs</Link></li>
-            <li className="nav-item"><Link to="/privacy" className="nav-link px-2 text-white">Privacy</Link></li>
-          </ul>
+                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li className="ms-3"><a className="text-body-secondary" href="#"><i className=" fs-4 text-white fab fa-instagram"></i></a></li>
+                <li className="ms-3"><a className="text-body-secondary" href="#"><i className=" fs-4 text-white fab fa-facebook"></i></a></li>
+                <li className="ms-3"><a className="text-body-secondary" href="#"></a></li>
+                </ul>
           <button id="myBtn" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}} className='btn btn-sm btn-outline-secondary'>
             <i className="bi bi-arrow-bar-up"></i>
           </button>
-        </footer>
-      </div>
+            </footer>
+        </div>
 
-    </div>
-  )
+)
 }
 
 export default Footer
