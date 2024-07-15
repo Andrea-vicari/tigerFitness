8b68c6dbef6ae6fcbeb9311365700f39daa7617b
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2024-07-15 08:03:57 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
     <React.Fragment>
       <ScrollToTop>
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/chisiamo" element={<AboutUsPage />} />
             <Route path="/servizi" element={<ServicePage />} />
             <Route path="/contatti" element={<ContactPage />} />
