bb0f2fcaa0aad31c39e672d0decc3f38c15c5f54
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-05-30 15:02:18 +0200

    Update UserProfile.jsx

--- client/src/Components/dashcomponents/UserProfile.jsx
+++ client/src/Components/dashcomponents/UserProfile.jsx
@@ -105,7 +105,7 @@ function UserProfile() {
     };
 
     axios
-      .patch(`https://pulsefit-server.vercel.app/api/users/aggiungi-immagine-utente/${singleUser}`, data)
+      .patch(`https://pulsefit-server.vercel.app/api/users/aggiungi-immagine-utente/$singleID`, data)
       .then((res) => {
         navigate(`/dashboardpage`);
       })
