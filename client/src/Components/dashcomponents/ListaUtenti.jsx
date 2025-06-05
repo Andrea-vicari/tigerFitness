94f39500b40729983258ec187f45b1c2355b655c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-05 16:33:41 +0200

    Update ListaUtenti.jsx

--- client/src/Components/dashcomponents/ListaUtenti.jsx
+++ client/src/Components/dashcomponents/ListaUtenti.jsx
@@ -1,4 +1,4 @@
-nteimport React from 'react'
+import React from 'react'
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
