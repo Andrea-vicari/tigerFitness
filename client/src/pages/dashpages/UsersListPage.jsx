25430d60f359ec9c3ebbd40b0ca1e992d5dfbf46
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-05 13:53:43 +0200

    Update UsersListPage.jsx

--- client/src/pages/dashpages/UsersListPage.jsx
+++ client/src/pages/dashpages/UsersListPage.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Footer from '../../Components/Common/Footer';
 import Navbar from '../../Components/Common/DashNav';
 import UsersList from '../../Components/dashcomponents/UsersList';
-
+import ListaUtenti from '../../Components/dashcomponents/ListaUtenti';
 
 
 function UsersListPage() {
   return (
     <>
     <Navbar />
-    <UsersList />
+    <ListaUtenti />
     <Footer />
     </>
   )
