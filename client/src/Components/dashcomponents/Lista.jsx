2f5b50b1961ce024fee8016dcd36e1e68f4454b4
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-09 16:52:50 +0200

    Update Lista.jsx

--- client/src/Components/dashcomponents/Lista.jsx
+++ client/src/Components/dashcomponents/Lista.jsx
@@ -34,7 +34,7 @@ const Lista = ({ utenti, loading }) => {
                   <Link to={`/modificacomponente/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
                     <i className='bi bi-zoom-in'></i>
                   </Link>
-                  <Link to={`/cancellacomponente/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
+                  <Link to={`/cancella-utente-page/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
                     <i className='bi bi-trash'></i>
                   </Link>
 
