c7d10a16fdee47d59ff0c8e5e2d407343caff081
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-09 16:03:59 +0200

    Update CancellaUtente.jsx

--- client/src/Components/dashcomponents/CancellaUtente.jsx
+++ client/src/Components/dashcomponents/CancellaUtente.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { useEffect } from "react";
 import {  Link, useLocation, Navigate, useNavigate } from 'react-router-dom';
-import logo from "../../assets/images/logo-autoparts-footer.svg";
+import logo from "../../assets/images/pulseFit_logo.svg";
 import axios from 'axios'
 
 var componenteDaCancID
