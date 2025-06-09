ceaa033ec4ee7136148c92bc865a59fe14c58913
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-09 16:26:59 +0200

    Update CancellaUtentePage.jsx

--- client/src/pages/dashpages/CancellaUtentePage.jsx
+++ client/src/pages/dashpages/CancellaUtentePage.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
+import CancellaUtente from '../../Components/dashcomponents/CancellaUtente'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+
+
+function CancellaUtentePage() {
+  return (
+    <>
+    <Navbar />
+    <CancellaUtente />
+    <Footer />
+    </>
+  )
+}
+
+export default CancellaUtentePage
 
