f19148360ec0697a805f2ea1c8358b9059fde55e
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-10 21:29:30 +0200

    Update Lista.jsx

--- client/src/Components/dashcomponents/Lista.jsx
+++ client/src/Components/dashcomponents/Lista.jsx
@@ -36,7 +36,7 @@ const Lista = ({ utenti, loading }) => {
               Aggiungi
               </Link>
                 </td>
-                <td className='pt-3'>{data.createdAt}</td>
+              
                    <td className='pt-3'>
                   <Link to={`/cancella-utente-page/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
                     <i className='bi bi-trash'></i>
