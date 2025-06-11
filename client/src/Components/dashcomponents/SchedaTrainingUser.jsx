2f77af58045a21e6381267524114db6a0ccb7918
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-11 14:43:04 +0200

    Update SchedaTrainingUser.jsx

--- client/src/Components/dashcomponents/SchedaTrainingUser.jsx
+++ client/src/Components/dashcomponents/SchedaTrainingUser.jsx
@@ -144,7 +144,8 @@ function SchedaTraining(){
                         <li className="list-group-item list-group-item-action d-flex gap-3" aria-current="true">
                       <div className="d-flex gap-2 w-100 justify-content-between">
                         <div>
-                        <h5 className="mb-3">Note: {e.notaAllenamento} KG</h5>
+                        <h5 className="mb-3">Note:</h5>
+                        <p>{e.notaAllenamento}</p>
                         </div>
                       </div>
                       </li>
