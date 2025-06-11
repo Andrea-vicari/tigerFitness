8b973e5c29e1baf5f359393751b9db4d3c8a5eee
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-11 16:00:18 +0200

    Create DettaglioUtente.jsx

---
+++ client/src/pages/dashpages/DettaglioUtente.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import SingleUser from '../../Components/dashcomponents/SingleUser';
+
+
+
+function DettaglioUtente() {
+  return (
+    <>
+    <Navbar />
+    <SingleUser />
+    <Footer />
+    </>
+  )
+}
+
+export default DettaglioUtente
