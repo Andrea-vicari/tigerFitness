f0fdbfe0fdb07f95870b95e563a898232bd6bee7
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-12 22:34:58 +0200

    Create StatistichePage.jsx

---
+++ client/src/pages/dashpages/StatistichePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import Statistiche from '../../Components/dashcomponents/Statistiche';
+
+function AgendaPage() {
+  return (
+    <>
+    <Navbar />
+    <Statistiche />
+    <Footer />
+    </>
+  )
+}
+
+export default AgendaPage
