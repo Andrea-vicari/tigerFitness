5aa2ba6875c93d864e637a12c9a308932f128fdf
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-16 15:06:04 +0200

    Update StatisticheUtente.jsx

--- client/src/Components/dashcomponents/StatisticheUtente.jsx
+++ client/src/Components/dashcomponents/StatisticheUtente.jsx
@@ -1,83 +1,78 @@
 import React from 'react'
 import { useSelector} from 'react-redux'
 
+function Prices() {
+    const themeType = useSelector((state) => state.counter.value)
 
-function StatisticheUtente() {
+    let bgType, textType;
 
-
-        const themeType = useSelector((state) => state.counter.value)
-
-        let bgType, textType, textColor;
-
-        themeType == "ligth" ? bgType = "bg-ligth" : bgType = "bg-dark"
-        themeType == "ligth" ? textType = "" : textType = "text-bg-dark"
-        themeType == "ligth" ? textColor = "" : textColor = "text-body-secondary"
-
-            // Declare a fixed length excerpt
-            var fixedLengthExcerpt;
-            // Function shorter() to short down a long description and generate an excerpt
-            // the length can be set by editing "arrayPh.length = x"
-            const shorter = (phrase) =>{
-            let arrayPh = phrase.split(' ');
-            arrayPh.length = 12;
-            fixedLengthExcerpt = arrayPh.join(' ');
-            return fixedLengthExcerpt
-            }
-            /*
-            // Run shorter() and add an excerpt to each object of local newsList json
-            for(let i=0;i<newsList.length;i++){
-                shorter(newsList[i].description)
-                newsList[i].fixedLengthExcerpt = fixedLengthExcerpt
-            }
-
-            // Declare a support array
-            var postShorted = []
-
-            for (let i=0;i<4;i++){
-                postShorted.push(newsList[i]);
-            }
-            */
+    themeType == "ligth" ? bgType = "bg-ligth" : bgType = "bg-dark"
+    themeType == "ligth" ? textType = "" : textType = "text-bg-dark"
 
   return (
-    <>
-    <section id="news" className={"pt-5 pb-3" + " " + bgType + " " + textType}>
+    <React.Fragment>
+        <div className={"container-fluid pb-5"+ " " + bgType + " " + textType} id='pricing'>
+        <h1 className="section-title pt-5">Our Prices</h1>
+        <p className='mb-5 text-center'>We offer great plans. Find the one that suits your needs.</p>
 
-    <div className="container">
-
-        <h1 className="section-title mt-5">Statistiche</h1>
-        <p className="section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-            {/*
-                    <div className="row pb-5">
-                    {postShorted.map((e)=>{
-                    return(
-                        <div className="col-md-6" key={e.title}>
-                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                <div className="col p-4 d-flex flex-column position-static">
-                                <strong className="d-inline-block mb-2 text-primary-emphasis">{e.category}</strong>
-                                <h3 className="mb-0">{e.title}</h3>
-
-                                <p className="card-text mb-auto">{e.fixedLengthExcerpt}</p>
-                                <a href="#" className="icon-link gap-1 icon-link-hover stretched-link">
-                                    Continue reading
-                                    <i className='bi bi-arrow-right'></i>
-                                </a>
-                                </div>
-                                <div className="col-auto d-none d-lg-block">
-                                <img src={e.thumbImage} className='bd-placeholder-img' style={{width:250,height:250}}/>
-                                </div>
-                            </div>
-                        </div>
-                    )})}
-
+        <div className='container'>
+            <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+                <div className="col">
+                    <div className="card mb-4 rounded-3 shadow-sm">
+                    <div className="card-header py-3">
+                        <h4 className="my-0 fw-normal">Free</h4>
                     </div>
-                    */}
-
-    </div>
-
-    </section>
-    </>
+                    <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$0<small className="text-body-secondary fw-light">/mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                        <li>10 users included</li>
+                        <li>2 GB of storage</li>
+                        <li>Email support</li>
+                        <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+                    </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card mb-4 rounded-3 shadow-sm">
+                    <div className="card-header py-3">
+                        <h4 className="my-0 fw-normal">Pro</h4>
+                    </div>
+                    <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$15<small className="text-body-secondary fw-light">/mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="w-100 btn btn-lg btn-primary">Get started</button>
+                    </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card mb-4 rounded-3 shadow-sm border-primary">
+                    <div className="card-header py-3 text-bg-primary border-primary">
+                        <h4 className="my-0 fw-normal">Enterprise</h4>
+                    </div>
+                    <div className="card-body">
+                        <h1 className="card-title pricing-card-title">$29<small className="text-body-secondary fw-light">/mo</small></h1>
+                        <ul className="list-unstyled mt-3 mb-4">
+                        <li>30 users included</li>
+                        <li>15 GB of storage</li>
+                        <li>Phone and email support</li>
+                        <li>Help center access</li>
+                        </ul>
+                        <button type="button" className="w-100 btn btn-lg btn-primary">Contact us</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </React.Fragment>
   )
 }
 
-export default StatisticheUtente
+export default Prices
