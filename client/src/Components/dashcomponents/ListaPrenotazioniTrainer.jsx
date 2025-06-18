1bce0ef02fc2d52c22365ccb9c6ff662c68f1fb3
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-18 14:57:01 +0200

    Update ListaPrenotazioniTrainer.jsx

--- client/src/Components/dashcomponents/ListaPrenotazioniTrainer.jsx
+++ client/src/Components/dashcomponents/ListaPrenotazioniTrainer.jsx
@@ -75,7 +75,7 @@ function ListaPrenotazioniTrainer () {
     <>
       <div className='container-fluid pt-1 mt-5 bg-stripe d-none d-sm-block'>
         <div className='container text-center mt-5 pb-1'>
-          <h1 className='display-2 text-white text-uppercase'>Elenco utenti</h1>
+          <h1 className='display-2 text-white text-uppercase'>Elenco prenotazioni</h1>
         </div>
       </div>
 
