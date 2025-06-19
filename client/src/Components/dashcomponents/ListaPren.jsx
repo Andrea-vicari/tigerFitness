26b81dfbb96980d0ff748eb7b4190dc01ca0b9e4
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-19 22:13:50 +0200

    Update ListaPren.jsx

--- client/src/Components/dashcomponents/ListaPren.jsx
+++ client/src/Components/dashcomponents/ListaPren.jsx
@@ -35,13 +35,13 @@ const ListaPren = ({ prenotazioni, loading }) => {
                     
                     
                    <td className='pt-3'>
-                      <Link type="button" to={`/newtraining/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
+                      <Link type="Link" to={`/newtraining/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
                          <i className="bi bi-check-square fs-4 mx-2"></i>
                       </Link>
                    </td>
               
                      <td className='pt-3'>
-                      <Link to={`/singleuser/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
+                      <Link to={`/singleuser/${data._id}`} state={data._id} type="Link" className="btn btn-sm btn-outline-danger mx-1">
                         <i className='bi bi-x-circle fs-4 mx-2'></i>
                       </Link>
                      </td>
