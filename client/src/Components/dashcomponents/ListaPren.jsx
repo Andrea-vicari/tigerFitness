2269155a93d83e5ee522f7881ec373e49f4249b5
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-19 22:07:47 +0200

    Update ListaPren.jsx

--- client/src/Components/dashcomponents/ListaPren.jsx
+++ client/src/Components/dashcomponents/ListaPren.jsx
@@ -25,34 +25,27 @@ const ListaPren = ({ prenotazioni, loading }) => {
             : prenotazioni.map((data, index) => (
                 <tr key={index}>
 
-               <td className='pt-3'>{data.user.email}</td>
-                <td className='pt-3'>{data.anno}</td>
+                    <td className='pt-3'>{data.user.email}</td>
+                    <td className='pt-3'>{data.anno}</td>
                     <td className='pt-3'>{data.mese}</td>
-                <td className='pt-3'>{data.giorno}</td>
+                    <td className='pt-3'>{data.giorno}</td>
                     <td className='pt-3'>{data.ora}</td>
                     <td className='pt-3'>{data.minuto}</td>
                     <td className='pt-3'>{data.status}</td>
                     
                     
                    <td className='pt-3'>
-                  <Link type="button" to={`/newtraining/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
-              <i className="bi bi-plus-square fs-4 mx-2"></i>
-                            </Link>
-                </td>
+                      <Link type="button" to={`/newtraining/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
+                         <i className="bi bi-check-square fs-4 mx-2"></i>
+                      </Link>
+                   </td>
               
-                   <td className='pt-3'>
-                  <Link to={`/singleuser/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
-                    <i className='bi bi-zoom-in fs-4 mx-2'></i>
-                  </Link>
-
-                </td>
-                 <td className='pt-3'>
-                  <Link to={`/cancella-utente-page/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
-                    <i className='bi bi-trash fs-4 mx-2'></i>
-                  </Link>
-
-                </td>
-
+                     <td className='pt-3'>
+                      <Link to={`/singleuser/${data._id}`} state={data._id} type="button" className="btn btn-sm btn-outline-danger mx-1">
+                        <i className='bi bi-x-circle fs-4 mx-2'></i>
+                      </Link>
+                     </td>
+                     <td className='pt-3'>
 
 
               </tr>
