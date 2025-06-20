241873f4ff9d67756fa9e4b05b4c557eb2ee0c5c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-20 14:33:13 +0200

    Update ConfermaPrenotazione.jsx

--- client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
+++ client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
@@ -5,9 +5,19 @@ import logo from "../../assets/images/pulseFit_logo.svg";
 import { useSelector } from 'react-redux'
 
 
+
 function ConfermaPrenotazione() {
 
+  const themeType = useSelector((state) => state.counter.value)
 
+  let bgType, textType;
+
+  themeType == "ligth" ? bgType = "bg-light" : bgType = "bg-dark"
+  themeType == "ligth" ? textType = "" : textType = "text-bg-dark"
+
+  let clicked = useLocation();
+
+  userID = clicked.state
 
 
 
