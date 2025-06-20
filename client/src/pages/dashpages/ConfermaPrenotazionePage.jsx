1ed258436b30d1514f7cb9bfff11e49355e86524
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-20 10:22:37 +0200

    Create ConfermaPrenotazionePage.jsx

---
+++ client/src/pages/dashpages/ConfermaPrenotazionePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import Agenda from '../../Components/dashcomponents/Agenda';
+
+function ConfermaPrenotazionePage() {
+  return (
+    <>
+    <Navbar />
+    <Agenda />
+    <Footer />
+    </>
+  )
+}
+
+export default ConfermaPrenotazionePage
