9ecc2bb0a1bdfef1d8510f4e2281025269539a87
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-20 14:29:48 +0200

    Update ConfermaPrenotazionePage.jsx

--- client/src/pages/dashpages/ConfermaPrenotazionePage.jsx
+++ client/src/pages/dashpages/ConfermaPrenotazionePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
 import Navbar from '../../Components/Common/DashNav';
-import Agenda from '../../Components/dashcomponents/ConfermaPrenotazione';
+import ConfermaPrenotazione from '../../Components/dashcomponents/ConfermaPrenotazione';
 
 function ConfermaPrenotazionePage() {
   return (
