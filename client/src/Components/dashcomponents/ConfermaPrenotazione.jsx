9e756631b557bc7c0ae857e29645aa4db8e618d3
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-23 15:28:16 +0200

    Update ConfermaPrenotazione.jsx

--- client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
+++ client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
@@ -63,13 +63,16 @@ function ConfermaPrenotazione() {
 
         <div className='container'>
             <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                <div className="col">
+              {prenotazione.map((e)=>{
+                   return(
+
+                <div className="col" key={e._id}>
                     <div className="card mb-4 rounded-3 shadow-sm">
                     <div className="card-header py-3">
                         <h4 className="my-0 fw-normal">Prenotazione</h4>
                     </div>
                     <div className="card-body">
-                        <h1 className="card-title pricing-card-title">$0<small className="text-body-secondary fw-light">/mo</small></h1>
+                        <h1 className="card-title pricing-card-title">$0</h1>
                         <ul className="list-unstyled mt-3 mb-4">
                         <li>10 users included</li>
                         <li>2 GB of storage</li>
@@ -80,6 +83,7 @@ function ConfermaPrenotazione() {
                     </div>
                     </div>
                 </div>
+                )})}       
 
 
             </div>
