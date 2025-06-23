573775be868e24a77c5066fa79bc6172f2aee588
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-23 15:50:41 +0200

    Update ConfermaPrenotazione.jsx

--- client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
+++ client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
@@ -74,10 +74,8 @@ function ConfermaPrenotazione() {
                     <div className="card-body">
                         <h3 className="card-title pricing-card-title">{e.status}</h3>
                         <ul className="list-unstyled mt-3 mb-4">
-                        <li>{e.giorno}</li>
-                        <li>{e.mese}</li>
-                        <li>{e.ora}</li>
-                        <li>{e.minuto}</li>
+                        <li>{e.giorno} {e.mese}</li>
+                        <li>{e.ora} {e.minuto}</li>
                         <li>{e.user.email}</li>
                         </ul>
                         <button type="button" className="w-100 btn btn-lg btn-outline-primary" onClick={()=>approvaPrenotazione()}>Conferma</button>
