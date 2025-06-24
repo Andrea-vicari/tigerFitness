33da8346d10e7e6848836a3e9a9249ca6d378b20
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-24 15:07:00 +0200

    Create RifiutaPrenotazionePage.jsx

---
+++ client/src/pages/dashpages/RifiutaPrenotazionePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+import RifiutaPrenotazione from '../../Components/dashcomponents/RifiutaPrenotazione';
+
+function RifiutaPrenotazionePage() {
+  return (
+    <>
+    <Navbar />
+    <RifiutaPrenotazione />
+    <Footer />
+    </>
+  )
+}
+
+export default RifiutaPrenotazionePage
