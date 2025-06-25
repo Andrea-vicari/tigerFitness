52d60efffe7a31c18dbbafda43177c735baaef88
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-25 16:25:50 +0200

    Update Agenda.jsx

--- client/src/Components/dashcomponents/Agenda.jsx
+++ client/src/Components/dashcomponents/Agenda.jsx
@@ -13,22 +13,89 @@ function Agenda() {
 
   return (
     <>
-    <section id="team" className={"Agenda pb-5" + " " + bgType + " " + textType}>
-        <div className="container mb-2 mt-5">
+     <section id="team" className={"team pb-5" + " " + bgType + " " + textType}>
+        <div className="container mb-2">
 
-            <div className="section-title pt-5 mt-5">
-                <h2 className="section-title">Agenda</h2>
-                <p className='mb-5 text-center fs-4>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
-                
+            <div>
+                <h2 className="section-title">il Team</h2>
+                <p className='mb-5 text-center fs-3'>Un piccolo Team di grande qualità al servizio del cliente</p>
+                <h4 className='text-center mb-5'>I nostri istruttori sono esperti nella progettazione di programmi di esercizi sicuri ed efficaci per individui con diversi livelli di forma fisica e condizioni di salute.</h4>
 
             </div>
 
             <div className="row">
-                
-                    
-                    
-                    
-                    
+
+                <div className={"col-lg-6 mb-3" + " " + bgType + " " + textType}>
+                    <div className={"member d-flex align-items-start"+ " " + darkType + " " + textType}>
+                        <div className="pic">
+                            <img src="https://placehold.co/400x400" className="img-fluid" alt=""/>
+                        </div>
+                        <div className="member-info">
+                        <h4 className='text-primary'>John Doe</h4>
+                        <span>Chief Executive Officer</span>
+                        <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                        <div className="social">
+                            <a href=""><i className="bi bi-instagram"></i></a>
+                            <a href=""><i className="bi bi-facebook"></i></a>
+                            <a href=""> <i className="bi bi-envelope"></i> </a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"col-lg-6 mb-3" + " " + bgType + " " + textType}>
+                    <div className={"member d-flex align-items-start"+ " " + darkType + " " + textType}>
+                        <div className="pic">
+                            <img src="https://placehold.co/400x400" className="img-fluid" alt=""/>
+                        </div>
+                        <div className="member-info">
+                        <h4 className='text-primary'>Jeff White</h4>
+                        <span>H.R. Manager</span>
+                        <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                        <div className="social">
+                            <a href=""><i className="bi bi-instagram"></i></a>
+                            <a href=""><i className="bi bi-facebook"></i></a>
+                            <a href=""> <i className="bi bi-envelope"></i> </a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"col-lg-6 mb-3" + " " + bgType + " " + textType}>
+                    <div className={"member d-flex align-items-start"+ " " + darkType + " " + textType}>
+                        <div className="pic">
+                            <img src="https://placehold.co/400x400" className="img-fluid" alt=""/>
+                        </div>
+                        <div className="member-info">
+                        <h4 className='text-primary'>Tom Reed</h4>
+                        <span>Product Manager</span>
+                        <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                        <div className="social">
+                            <a href=""><i className="bi bi-instagram"></i></a>
+                            <a href=""><i className="bi bi-facebook"></i></a>
+                            <a href=""> <i className="bi bi-envelope"></i> </a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"col-lg-6 mb-3" + " " + bgType + " " + textType}>
+                    <div className={"member d-flex align-items-start"+ " " + darkType + " " + textType}>
+                        <div className="pic">
+                            <img src="https://placehold.co/400x400" className="img-fluid" alt=""/>
+                        </div>
+                        <div className="member-info">
+                        <h4 className='text-primary'>Shawn Stephen</h4>
+                        <span>Finance Manager</span>
+                        <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                        <div className="social">
+                            <a href=""><i className="bi bi-instagram"></i></a>
+                            <a href=""><i className="bi bi-facebook"></i></a>
+                            <a href=""> <i className="bi bi-envelope"></i> </a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
