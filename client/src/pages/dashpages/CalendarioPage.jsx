a67f3b36f3073daf1a742c27c442a306fc7dba3b
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-26 15:23:33 +0200

    Create CalendarioPage.jsx

---
+++ client/src/pages/dashpages/CalendarioPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Calendario from '../../Components/dashcomponents/Calendario'
+import Footer from '../../Components/Common/Footer';
+import Navbar from '../../Components/Common/DashNav';
+
+
+function CalendarioPage() {
+  return (
+    <>
+    <Navbar />
+    <Calendario />
+    <Footer />
+    </>
+  )
+}
+
+export default CalendarioPage
