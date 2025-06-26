e084a09a0837e1318a925557abe779f642526c53
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-06-26 16:53:28 +0200

    Update Calendario.jsx

--- client/src/Components/dashcomponents/Calendario.jsx
+++ client/src/Components/dashcomponents/Calendario.jsx
@@ -27,38 +27,11 @@ function Calendario() {
 
         <div className='container'>
             <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                  <div className="month">
-  <ul>
-    <li className="prev">&#10094;</li>
-    <li className="next">&#10095;</li>
-    <li>August<br><span style="font-size:18px">2021</span></li>
-  </ul>
-</div>
+                  
 
-<ul className="weekdays">
-  <li>Mo</li>
-  <li>Tu</li>
-  <li>We</li>
-  <li>Th</li>
-  <li>Fr</li>
-  <li>Sa</li>
-  <li>Su</li>
-</ul>
 
-<ul className="days">
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-  <li>4</li>
-  <li>5</li>
-  <li>6</li>
-  <li>7</li>
-  <li>8</li>
-  <li>9</li>
-  <li><span className="active">10</span></li>
-  <li>11</li>
-  
-</ul>
+
+
                     
                    
                     
