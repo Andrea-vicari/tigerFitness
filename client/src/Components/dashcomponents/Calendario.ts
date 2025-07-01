9d0e90d92d3b1ede66ed568d48565903f3e5dc9c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-01 15:45:45 +0200

    Update Calendario.ts

--- client/src/Components/dashcomponents/Calendario.ts
+++ client/src/Components/dashcomponents/Calendario.ts
@@ -13,14 +13,7 @@ export default function Calendario() {
 
   return (
     <div className="Sample">
-      <header>
-        <h1>react-calendar sample page</h1>
-      </header>
-      <div className="Sample__container">
-        <main className="Sample__container__content">
-          <Calendar onChange={onChange} showWeekNumbers value={value} />
-        </main>
-      </div>
+      <Calendar />
     </div>
   );
 }
