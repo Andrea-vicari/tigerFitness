aeffe9e97a6524b6e6c0d28f2e0c50405b14eb1b
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-02 14:59:35 +0200

    Update Calendario.jsx

--- client/src/Components/dashcomponents/Calendario.jsx
+++ client/src/Components/dashcomponents/Calendario.jsx
@@ -17,9 +17,7 @@ const Calendario = () => {
     const days = daysInMonth(year, month);
     const firstDay = new Date(year, month, 1).getDay();
 
-    setMese(month)
-    //console.log("mese dopo il primo set")
-    //console.log(mese)
+  
     const calendarDays = [];
     for (let i = 0; i < firstDay; i++) {
       calendarDays.push(<div key={`empty-${i}`} className="empty-day"></div>);
