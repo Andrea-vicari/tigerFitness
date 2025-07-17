ea95f7efa92ed1ddf1900faa07db387e0f21e272
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-17 13:57:01 +0200

    Update ListaPrenUs.jsx

--- client/src/Components/dashcomponents/ListaPrenUs.jsx
+++ client/src/Components/dashcomponents/ListaPrenUs.jsx
@@ -31,21 +31,7 @@ const ListaPrenUs = ({ prenotazioni, loading }) => {
                     <td className='pt-3'>{data.giorno}</td>
                     <td className='pt-3'>{data.ora}</td>
                     <td className='pt-3'>{data.minuto}</td>
-                    <td className='pt-3'>{data.status}</td>
-                    
-                    
-                   <td className='pt-3'>
-                      <Link type="Link" to={`/conferma-prenotazione/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
-                         <i className="bi bi-check-square fs-4 mx-2"></i>
-                      </Link>
-                   </td>
-              
-                     <td className='pt-3'>
-                      <Link type="Link" to={`/rifiuta-prenotazione/${data._id}`} state={data._id} className="btn btn-outline-danger fs-6">
-                        <i className='bi bi-x-circle fs-4 mx-2'></i>
-                      </Link>
-                     </td>
-                  
+                    <td className='pt-3'>{data.status}</td>      
 
 
               </tr>
