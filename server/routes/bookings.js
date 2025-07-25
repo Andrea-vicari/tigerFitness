330eed4443e32b351a2bd62910423dba49e3a4d8
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-25 14:13:17 +0200

    Update bookings.js

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {viewAllBookings, createNewBooking, viewSingleBooking, confermaPrenotazione, viewBookingTBA, vediApprovate, viewSingolaPren} = require('../controllers/bookingController');
+const {viewAllBookings, createNewBooking, viewSingleBooking, aggiornaPrenotazione, viewBookingTBA, vediApprovate, viewSingolaPren} = require('../controllers/bookingController');
 
 
 const router = express.Router();
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', viewAllBookings);
 router.post('/', createNewBooking);
 router.get('/prenotazioni-singolo-utente/:id', viewSingleBooking)
-router.patch('/conferma-prenotazione/:id', confermaPrenotazione)
+router.patch('/conferma-prenotazione/:id', aggiornaPrenotazione)
 router.get('/prenotazioni-da-approvare', viewBookingTBA)
 router.get('/prenotazioni-approvate', vediApprovate)
 router.get('/vedi-singola-pren/:id', viewSingolaPren)
