47dbbf3778018ce6cbf6ff9866edb395abb9b386
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-25 13:54:15 +0200

    Update bookingController.js

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -74,7 +74,7 @@ const vediApprovate = async (req, res) =>{
     res.status(200).json(prenotApprovate)
 }
 
-const updateStatusBooking = async (req, res) =>{
+const confermaPrenotazione = async (req, res) =>{
     const {status } = req.body
     const { id } = req.params;
     const singleBooking = await Bookings.findOneAndUpdate({"_id":id},{
@@ -103,7 +103,7 @@ module.exports = {
     viewAllBookings,
     createNewBooking,
     viewSingleBooking,
-    updateStatusBooking,
+    confermaPrenotazione,
     viewBookingTBA,
     vediApprovate,
     viewSingolaPren
