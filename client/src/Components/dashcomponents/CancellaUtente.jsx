d5ba091b8615f3e33bc8f4315c475726c82c5e76
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-29 14:32:55 +0200

    Update CancellaUtente.jsx

--- client/src/Components/dashcomponents/CancellaUtente.jsx
+++ client/src/Components/dashcomponents/CancellaUtente.jsx
@@ -39,7 +39,7 @@ function CancellaUtente() {
 
 	openModal()
 
-  axios.delete(`https://pulsefit-server.vercel.app/api/users/cancella-singolo-utente/${utenteDaCancID}`)
+  axios.delete(`https://tiger-fitness.vercel.app/api/users/cancella-singolo-utente/${utenteDaCancID}`)
 
 
  }
