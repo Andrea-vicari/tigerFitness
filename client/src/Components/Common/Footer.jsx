dee96aa67b73c2cde53917836c5a2cf3fae65a1c
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-29 21:34:13 +0200

    Update Footer.jsx

--- client/src/Components/Common/Footer.jsx
+++ client/src/Components/Common/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useState} from 'react';
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/tiger-fitness-logo.svg"
+import logo from "../../assets/images/tiger-fitness-logo-footer.svg"
 
 function Footer() {
 
