7362348582e39d96e1bb9aebaa31c40d0ded83ae
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-07-30 13:06:41 +0200

    Update ConfermaPrenotazione.jsx

--- client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
+++ client/src/Components/dashcomponents/ConfermaPrenotazione.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/images/pulseFit_logo.svg";
+import logo from "../../assets/images/tiger-fitness-logo.svg";
 import { UseAuthContext } from "../../hooks/UseAuthContext"
 import { useSelector } from 'react-redux'
 
