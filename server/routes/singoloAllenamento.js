46de460e669e8d5beaa30687f9ff22cc20e301fb
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-08-28 14:38:09 +0200

    Update singoloAllenamento.js

--- server/routes/singoloAllenamento.js
+++ server/routes/singoloAllenamento.js
@@ -1,1 +1,13 @@
+const express = require('express');
 
+const {creaAllenamentoSingolo} = require('../controllers/creaAllenamentoSingolo');
+
+
+const router = express.Router();
+
+
+router.post('/', creaAllenamentoSingolo);
+
+
+
+module.exports = router;
