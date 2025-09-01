8f995e3de762670f578fe674a4f65a238d017185
Andrea Vicari <120132265+Andrea-vicari@users.noreply.github.com>
2025-09-01 14:49:35 +0200

    Update TuttiWorkoutPage.jsx

--- client/src/pages/dashpages/TuttiWorkoutPage.jsx
+++ client/src/pages/dashpages/TuttiWorkoutPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Footer from '../../Components/Common/Footer';
 import Navbar from '../../Components/Common/DashNav';
-import ListaUtenti from '../../Components/dashcomponents/ListaUtenti';
+import ListaTuttiWorkout from '../../Components/dashcomponents/ListaTuttiWorkout';
 
 
 function TuttiWorkoutPage() {
   return (
     <>
     <Navbar />
-    <ListaUtenti />
+    <ListaTuttiWorkout />
     <Footer />
     </>
   )
